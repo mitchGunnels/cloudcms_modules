962691b31de4d9572723ea9db2814a720f173c25
Mitchell Gunnels <mg717k@att.com>
2018-02-06 17:33:55 -0500

    uncomment code

--- theme.js
+++ theme.js
@@ -1,9 +1,9 @@
 define(function(require, exports, module) {
 
-    //require("css!bootstrap/../../css/bootstrap.css");
-    //require("css!app/../../main.css");
+    require("css!bootstrap/../../css/bootstrap.css");
+    require("css!app/../../main.css");
 
-    //require("css!themes/theme-reset.css");
+    require("css!themes/theme-reset.css");
     require("css!./theme.css");
 
 });

5511809ea1124954e51ad6b9db95fb049ba47b22
Mitchell Gunnels <mg717k@att.com>
2018-02-10 09:20:58 -0500

    discovery about UI

--- preview-url/index.js
+++ preview-url/index.js
@@ -1,5 +1,9 @@
 define(function(require, exports, module) {
 	//var moduleId = module.uri.match(/^.+(_modules[^\/]+)\/.*/)[1];
 	console.log('module', module);
+
+	var UI = require("ui");
+	console.log(UI);
+
     require("./script/preview.js");
 });

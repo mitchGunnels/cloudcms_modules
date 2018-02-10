58faf3d1a914204ee5828ef95d07300f39d8fae1
Mitchell Gunnels <mg717k@att.com>
2018-02-10 00:07:03 -0500

    test

--- preview-url/script/preview.js
+++ preview-url/script/preview.js
@@ -2,9 +2,10 @@ define(function(require, exports, module) {
     var $ = require("jquery");
     $(document).ajaxStop(function() {
         // 0 === $.active
+        //Must delay for page render after ajax finishes. 
         setTimeout(function() {
             var inputVal = $('div[name=previewURL]').text();
             console.log(inputVal);
-        }, 1000);
+        }, 500);
     });
 });

5b8273544a10d6c70ba1b76234920b0a158c4ba2
Mitchell Gunnels <mg717k@att.com>
2018-02-10 00:24:29 -0500

    env selction

--- preview-url/script/preview.js
+++ preview-url/script/preview.js
@@ -1,7 +1,17 @@
 define(function(require, exports, module) {
     var $ = require("jquery");
     $(document).ajaxStop(function() {
-        // 0 === $.active
+        //Check workspace-picker to determine the appropriate env for creating the preview link.
+        var workspacePickerVal = $('select.workspace-picker option:selected').val();
+        //Decide on the correct environment
+        if (workspacePickerVal.contains("master")) {
+            console.log("prod");
+        } else if (workspacePickerVal.contains("SIT1")) {
+            console.log("SIT1");
+        } else {
+            console.log("Nothing found");
+        }
+
         //Must delay for page render after ajax finishes. 
         setTimeout(function() {
             var inputVal = $('div[name=previewURL]').text();

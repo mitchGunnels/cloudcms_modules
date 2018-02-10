f0cc7628f9b4b2ef6782b8a489ae8c1a743f1ebb
Mitchell Gunnels <mg717k@att.com>
2018-02-10 00:04:21 -0500

    test

--- preview-url/script/preview.js
+++ preview-url/script/preview.js
@@ -1,15 +1,10 @@
 define(function(require, exports, module) {
     var $ = require("jquery");
-
     $(document).ajaxStop(function() {
         // 0 === $.active
-        //Have to wait for ajax to stop and render page...delay 1ms for render.
         setTimeout(function() {
             var inputVal = $('div[name=previewURL]').text();
             console.log(inputVal);
-        }, 1);
-
+        }, 1000);
     });
-
-
 });

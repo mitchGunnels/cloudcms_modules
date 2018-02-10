d16773e3ba8bcfac3915c2be86518415ed5ae49b
Mitchell Gunnels <mg717k@att.com>
2018-02-10 09:46:01 -0500

    clean up

--- preview-url/index.js
+++ preview-url/index.js
@@ -1,13 +1,5 @@
 define(function(require, exports, module) {
 	//var moduleId = module.uri.match(/^.+(_modules[^\/]+)\/.*/)[1];
-	console.log('module', module);
-
-	var UI = require("ui");
-	console.log(UI.showModal);
-
-	UI.showModal({
-		"title":"test"
-	});
-
+	//console.log('module', module);
     require("./script/preview.js");
 });

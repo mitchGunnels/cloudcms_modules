fb9f8062d8445fa658a194272ab19040e82dce5c
Mitchell Gunnels <mg717k@att.com>
2018-02-09 23:24:00 -0500

    console

--- alert/index.js
+++ alert/index.js
@@ -1,3 +1,5 @@
-define(function(require) {
+define(function(require, exports, module) {
+	var moduleId = module.uri.match(/^.+(_modules[^\/]+)\/.*/)[1];
+	console.log('module', module);
     require("./test/alert.js");
 });

5943b3a3bb7025e9ec8f8e80f30690e528fd6e15
Mitchell Gunnels <mg717k@att.com>
2018-02-20 20:59:15 -0500

    remove styles

--- ckeditor-mods/script/ckeditor-mods.js
+++ ckeditor-mods/script/ckeditor-mods.js
@@ -12,5 +12,6 @@ define(function(require, exports, module) {
     CKEDITOR.config.disallowedContent = 'script; style';
     CKEDITOR.config.enterMode = CKEDITOR.ENTER_BR;
     //CKEDITOR.config.pasteFromWordCleanupFile = 'plugins/pastefromword/filter/custom.js';
+    CKEDITOR.config.pasteFromWordRemoveStyles = true;
     CKEDITOR.config.pasteFromWordPromptCleanup = true;
 });

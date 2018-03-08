ef4c6ca9764682f8da3535d487fedd07af79e16c
Mitchell Gunnels <mg717k@att.com>
2018-03-08 16:43:48 -0500

    fix location

--- ckeditor-mods/script/ckeditor-mods.js
+++ ckeditor-mods/script/ckeditor-mods.js
@@ -10,8 +10,8 @@ define(function(require, exports, module) {
         }
     };
 
-    CKEDITOR.plugins.addExternal( 'balloonpanel', 'https://cache.cricketwireless.com/ckeditor/balloonpanel/');
-    CKEDITOR.plugins.addExternal( 'a11ychecker', 'https://cache.cricketwireless.com/ckeditor/');
+    CKEDITOR.plugins.addExternal( 'balloonpanel', 'https://cache.cricketwireless.com/ckeditor-plugins/balloonpanel/');
+    CKEDITOR.plugins.addExternal( 'a11ychecker', 'https://cache.cricketwireless.com/ckeditor-plugins/');
 
     CKEDITOR.config.extraPlugins = 'cloudcms-image,balloonpanel,a11ychecker';
     CKEDITOR.config.enterMode = CKEDITOR.ENTER_BR;

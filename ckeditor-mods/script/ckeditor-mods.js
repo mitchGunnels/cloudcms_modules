1ef224c5fac4aedee30002efb6fd919aa2483789
Mitchell Gunnels <mg717k@att.com>
2018-03-09 16:43:45 -0500

    test

--- ckeditor-mods/script/ckeditor-mods.js
+++ ckeditor-mods/script/ckeditor-mods.js
@@ -14,6 +14,35 @@ define(function(require, exports, module) {
         }
     };
 
+    CKEDITOR.config.toolbarGroups = [
+        { name: 'clipboard', groups: ['clipboard', 'undo'] },
+        { name: 'tools', groups: ['tools'] },
+        { name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
+        { name: 'forms', groups: ['forms'] },
+        { name: 'paragraph', groups: ['align', 'list', 'indent', 'blocks', 'bidi', 'paragraph'] },
+        { name: 'links', groups: ['links'] },
+        { name: 'insert', groups: ['insert'] },
+        { name: 'styles', groups: ['styles'] },
+        { name: 'colors', groups: ['colors'] },
+        { name: 'others', groups: ['others'] },
+        { name: 'about', groups: ['about'] },
+        { name: 'editing', groups: ['spellchecker', 'find', 'selection', 'editing'] },
+        { name: 'document', groups: ['mode', 'document', 'doctools'] }
+    ];
+
+
+    CKEDITOR.config.removeButtons = 'Save,NewPage,Preview,Templates,ShowBlocks,Cut,Copy,Paste,PasteText,PasteFromWord,SelectAll,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Replace,Find,CopyFormatting,RemoveFormat,BidiLtr,BidiRtl,Language,CreateDiv,Flash,Image,Smiley,PageBreak,Iframe,About,TextColor,BGColor,FontSize,Font,Format';
+    CKEDITOR.config.enterMode = CKEDITOR.ENTER_BR;
+    CKEDITOR.config.pasteFromWordRemoveStyles = true;
+    CKEDITOR.config.pasteFromWordRemoveFontStyles = true;
+    CKEDITOR.config.entities_processNumerical = true;
+    CKEDITOR.config.disallowedContent = 'script; style; *[on*, border, width, height, cellpadding, valign, cellspacing, font, style]; *{*}';
+
+
+
+
+
+
 
     CKEDITOR.plugins.add('globalContent', {
         init: function(editor) {
@@ -31,36 +60,13 @@ define(function(require, exports, module) {
                 label: 'Insert global content',
                 command: pluginName,
                 className: 'cke_button__find_icon',
+                toolbar: 'basicstyles,3'
             });
         }
     });
 
 
     CKEDITOR.config.extraPlugins = 'globalContent';
-
-    CKEDITOR.config.toolbarGroups = [
-        { name: 'clipboard', groups: ['clipboard', 'undo'] },
-        { name: 'tools', groups: ['tools'] },
-        { name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
-        { name: 'forms', groups: ['forms'] },
-        { name: 'paragraph', groups: ['align', 'list', 'indent', 'blocks', 'bidi', 'paragraph'] },
-        { name: 'links', groups: ['links'] },
-        { name: 'insert', groups: ['insert'] },
-        { name: 'styles', groups: ['styles'] },
-        { name: 'colors', groups: ['colors'] },
-        { name: 'others', groups: ['others'] },
-        { name: 'about', groups: ['about'] },
-        { name: 'editing', groups: ['spellchecker', 'find', 'selection', 'editing'] },
-        { name: 'document', groups: ['mode', 'document', 'doctools','globalContent'] }
-    ];
-
-
-    CKEDITOR.config.removeButtons = 'Save,NewPage,Preview,Templates,ShowBlocks,Cut,Copy,Paste,PasteText,PasteFromWord,SelectAll,Form,Checkbox,Radio,TextField,Textarea,Select,Button,ImageButton,HiddenField,Replace,Find,CopyFormatting,RemoveFormat,BidiLtr,BidiRtl,Language,CreateDiv,Flash,Image,Smiley,PageBreak,Iframe,About,TextColor,BGColor,FontSize,Font,Format';
-    CKEDITOR.config.enterMode = CKEDITOR.ENTER_BR;
-    CKEDITOR.config.pasteFromWordRemoveStyles = true;
-    CKEDITOR.config.pasteFromWordRemoveFontStyles = true;
-    CKEDITOR.config.entities_processNumerical = true;
-    CKEDITOR.config.disallowedContent = 'script; style; *[on*, border, width, height, cellpadding, valign, cellspacing, font, style]; *{*}';
 
 
 

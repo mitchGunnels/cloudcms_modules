70a4ced8a9268d421ceac4d96e8c68e8653053a1
Mitchell Gunnels <mg717k@att.com>
2018-03-12 13:00:14 -0400

    test

--- ckeditor-mods/script/ckeditor-mods.js
+++ ckeditor-mods/script/ckeditor-mods.js
@@ -42,26 +42,7 @@ define(function(require, exports, module) {
 
 
 
-    var dialogObj = CKEDITOR.dialog.add('testOnly', function(editor) {
-        return {
-            title: 'Test Dialog',
-            resizable: CKEDITOR.DIALOG_RESIZE_BOTH,
-            minWidth: 500,
-            minHeight: 400,
-            contents: [{
-                id: 'tab1',
-                label: 'First Tab',
-                title: 'First Tab Title',
-                accessKey: 'Q',
-                elements: [{
-                    type: 'text',
-                    label: 'Test Text 1',
-                    id: 'testText1',
-                    'default': 'hello world!'
-                }]
-            }]
-        };
-    });
+    var dialogObj = new CKEDITOR.dialog( editor, 'smiley' );
 
 
     CKEDITOR.plugins.add('globalContent', {

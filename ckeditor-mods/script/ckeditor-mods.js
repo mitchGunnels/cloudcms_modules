a8af3f699bd82c6d40c84b1b9a18cd1c214c4d46
Mitchell Gunnels <mg717k@att.com>
2018-03-13 22:04:04 -0400

    fix

--- ckeditor-mods/script/ckeditor-mods.js
+++ ckeditor-mods/script/ckeditor-mods.js
@@ -1,7 +1,7 @@
 define(function(require, exports, module) {
     var modalContent;
     var modalHtml = '<div class="fade modal"role=dialog id=globalContent tabindex=-1><div class=modal-dialog role=document><div class=modal-content><div class=modal-header><button class=close type=button data-dismiss=modal aria-label=Close><span aria-hidden=true>×</span></button><h4 class=modal-title>Insert Modal</h4></div><div class=modal-body><p><form><div class=form-group><label for=searchTerm>Modal Search (by title)</label><input class="form-control input-lg"id=searchTerm placeholder="Modal title"type=input></div><div id=result></div></form></p></div><div class=modal-footer><button class="btn btn-default"type=button data-dismiss=modal>Close</button> <button class="btn btn-primary" type="button" id="insert">Insert</button></div></div></div></div>';
-    var modalCSS = 'body div.modal-backdrop, body div.modal-backdrop.fade.in { z-index: 9997 !important; } .cke_button__globalcontent_icon { display: none; } .cke_button__globalcontent_label { display: inline !important; padding: 0px; margin: 0px; } .modal.fade, .modal-scrollable { z-index: 9998 !important; } span#modalID { font-size: 11px; font-style: italic; } .autocomplete-suggestions { border: 1px solid #999; background: #FFF; overflow: auto; z-index: 9999 !important; } .autocomplete-suggestion { padding: 2px 5px; white-space: nowrap; overflow: hidden; } .autocomplete-selected { background: #F0F0F0; } .autocomplete-suggestions strong { font-weight: normal; color: #3399FF; } .autocomplete-group { padding: 2px 5px; } .autocomplete-group strong { display: block; border-bottom: 1px solid #000; }';
+    var modalCSS = 'body div.modal-backdrop, body div.modal-backdrop.fade.in { z-index: 9997 !important; } .cke_button__globalcontent_icon, .cke_button__legalcontent_icon { display: none; } .cke_button__globalcontent_label, .cke_button__legalcontent_label { display: inline !important; padding: 0px; margin: 0px; } .modal.fade, .modal-scrollable { z-index: 9998 !important; } span#modalID { font-size: 11px; font-style: italic; } .autocomplete-suggestions { border: 1px solid #999; background: #FFF; overflow: auto; z-index: 9999 !important; } .autocomplete-suggestion { padding: 2px 5px; white-space: nowrap; overflow: hidden; } .autocomplete-selected { background: #F0F0F0; } .autocomplete-suggestions strong { font-weight: normal; color: #3399FF; } .autocomplete-group { padding: 2px 5px; } .autocomplete-group strong { display: block; border-bottom: 1px solid #000; }';
     var $ = require("jquery");
     var uri = module.uri;
     uri = uri.substring(0, uri.lastIndexOf('/'));
@@ -33,7 +33,8 @@ define(function(require, exports, module) {
         { name: 'about', groups: ['about'] },
         { name: 'editing', groups: ['spellchecker', 'find', 'selection', 'editing'] },
         { name: 'document', groups: ['mode', 'document', 'doctools'] },
-        { name: 'globalContent' }
+        { name: 'globalContent' },
+        { name: 'legalContent' }
     ];
 
 
@@ -47,9 +48,9 @@ define(function(require, exports, module) {
     CKEDITOR.plugins.add('globalContent', {
         init: function(editor) {
 
-            var pluginName = 'globalContent';
+            var modalContent = 'modalContent';
 
-            editor.addCommand(pluginName, {
+            editor.addCommand(modalContent, {
                 exec: function(editor) {
                     // var eName = editor.name;
                     // var selectedEditor = CKEDITOR.instances.eName;
@@ -78,16 +79,16 @@ define(function(require, exports, module) {
 
             editor.ui.addButton('globalContent', {
                 label: 'Modal',
-                command: pluginName,
-                className: 'cke_button_label cke_button__modal_label',
+                command: modalContent,
+                //className: 'cke_button_label cke_button__modal_label',
                 toolbar: 'globalContent,1'
             });
 
             editor.ui.addButton('legalContent', {
                 label: 'Legal',
-                command: pluginName,
-                className: 'cke_button_label cke_button__modal_label',
-                toolbar: 'globalContent,2'
+                command: modalContent,
+                //className: 'cke_button_label cke_button__modal_label',
+                toolbar: 'legalContent,1'
             });
         }
     });

99cf468b2c4b1a1326b055792f01f191364d12f7
Mitchell Gunnels <mg717k@att.com>
2018-03-13 22:36:58 -0400

    legal

--- ckeditor-mods/script/ckeditor-mods.js
+++ ckeditor-mods/script/ckeditor-mods.js
@@ -1,6 +1,7 @@
 define(function(require, exports, module) {
     var modalContent;
-    var modalHtml = '<div class="fade modal"role=dialog id=globalContent tabindex=-1><div class=modal-dialog role=document><div class=modal-content><div class=modal-header><button class=close type=button data-dismiss=modal aria-label=Close><span aria-hidden=true>×</span></button><h4 class=modal-title>Insert Modal</h4></div><div class=modal-body><p><form><div class=form-group><label for=searchTerm>Modal Search (by title)</label><input class="form-control input-lg"id=searchTerm placeholder="Modal title"type=input></div><div id=result></div></form></p></div><div class=modal-footer><button class="btn btn-default"type=button data-dismiss=modal>Close</button> <button class="btn btn-primary" type="button" id="insert">Insert</button></div></div></div></div>';
+    var modalHtml = '<div id="globalContent" class="fade modal" role="dialog" tabindex="-1"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"> <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button><h4 class="modal-title" id="insert">Insert Modal</h4></div><div class="modal-body"><p><form><div class="form-group"> <label for="searchTerm">Modal Search (by title)</label> <input class="form-control input-lg" id="searchTerm" placeholder="Modal title" type="input" /></div><div id="result"></div></form></p></div><div class="modal-footer"> <button class="btn btn-default" type="button" data-dismiss="modal">Close</button> <button class="btn btn-primary" type="button">Insert</button></div></div></div></div>';
+    var legalHtml = '<div id="legalContent" class="fade modal" role="dialog" tabindex="-1"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"> <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button><h4 class="modal-title" id="legalInsert">Insert Legal Content</h4></div><div class="modal-body"><p><form><div class="form-group"> <label for="legalSearch">Legal Search (by topic)</label> <input class="form-control input-lg" id="legalSearch" placeholder="Legal topic" type="input" /></div><div id="legalResult"></div></form></p></div><div class="modal-footer"> <button class="btn btn-default" type="button" data-dismiss="modal">Close</button> <button class="btn btn-primary" type="button">Insert</button></div></div></div></div>';
     var modalCSS = 'body div.modal-backdrop, body div.modal-backdrop.fade.in { z-index: 9997 !important; } .cke_button__globalcontent_icon, .cke_button__legalcontent_icon { display: none; } .cke_button__globalcontent_label, .cke_button__legalcontent_label { display: inline !important; padding: 0px; margin: 0px; } .modal.fade, .modal-scrollable { z-index: 9998 !important; } span#modalID { font-size: 11px; font-style: italic; } .autocomplete-suggestions { border: 1px solid #999; background: #FFF; overflow: auto; z-index: 9999 !important; } .autocomplete-suggestion { padding: 2px 5px; white-space: nowrap; overflow: hidden; } .autocomplete-selected { background: #F0F0F0; } .autocomplete-suggestions strong { font-weight: normal; color: #3399FF; } .autocomplete-group { padding: 2px 5px; } .autocomplete-group strong { display: block; border-bottom: 1px solid #000; }';
     var $ = require("jquery");
     var uri = module.uri;
@@ -71,21 +72,19 @@ define(function(require, exports, module) {
 
             editor.addCommand(legalContent, {
                 exec: function(editor) {
-
-                    alert('legalContent');
-                   /* $('#insert').unbind("click");
-                    $('#globalContent').modal('show');
-                    $('#insert').on('click', function(event) {
+                   $('#legalInsert').unbind("click");
+                    $('#legalContent').modal('show');
+                    $('#legalInsert').on('click', function(event) {
                         event.preventDefault();
-                        var modalTitle = $('#result h4#modalTitle').text();
-                        var modalID = $('#result span#modalID').text();
+                        //var modalTitle = $('#result h4#modalTitle').text();
+                        //var modalID = $('#result span#modalID').text();
                         
-                        editor.insertHtml('<a href="' + modalID + '" title="" class="custom-class" data-toggle="modal" data-target="' + modalID + '">' + modalTitle + '</a>');
+                        //editor.insertHtml('<a href="' + modalID + '" title="" class="custom-class" data-toggle="modal" data-target="' + modalID + '">' + modalTitle + '</a>');
                         
-                        $('#globalContent').modal('hide');
-                        $('#globalContent #result').empty();
-                        $('#searchTerm').val('');
-                    }); */
+                        $('#legalContent').modal('hide');
+                        $('#legalContent #legalResult').empty();
+                        $('#legalSearch').val('');
+                    });
                 },
                 canUndo: true
             });
@@ -114,7 +113,10 @@ define(function(require, exports, module) {
         editor.on('pluginsLoaded', function() {
             // If our custom dialog has not been registered, do that now.
             if ($('#globalContent').length == 0) {
+
                 $('body').append(modalHtml);
+                $('body').append(legalHtml);
+
                 $('<style>' + modalCSS + '</style>').appendTo('#globalContent');
                 $('select.workspace-picker').on('change', initAutoComplete);
                 initAutoComplete();
@@ -156,7 +158,7 @@ define(function(require, exports, module) {
 
         //console.log('GET CALLED!');
         //EVENTUALLY NEED TO SEARCH ONDEMAND, WILL NEED TO MODIFY THE MIDDLEWARE
-        $.get(domain + '/cloudassets/cms/myAccount/modal', function(result) {
+        $.get(domain + '/cloudassets/cms/modal/content', function(result) {
             var newObject = [];
             $.each(result, function(data) {
                 //console.log(data);
@@ -167,6 +169,18 @@ define(function(require, exports, module) {
             modalContent = newObject;
             searchInit();
         });
+
+        //LOAD LEGAL CONTENT
+        $.get(domain + '/cloudassets/cms/legal/content', function(result) {
+            var newObject = [];
+            $.each(result, function(data) {
+                //var dataObj = { "value": this.title, "data": { "ID": data, "title": this.modalTitle, "modalBody": this.modalBody } };
+                //newObject.push(dataObj);
+            });
+
+            legalContent = newObject;
+            legalInit();
+        });
     }
 
     function searchInit() {
@@ -174,6 +188,14 @@ define(function(require, exports, module) {
             lookup: modalContent,
             onSelect: function(suggestion) {
                 $('#result').empty().html('<h4 id="modalTitle">' + suggestion.value + '</h4><p id="modalBody">' + suggestion.data.modalBody + '<br><br><span id="modalID">' + suggestion.data.ID + '</span></p>');
+            }
+        });
+    }
+    function legalInit() {
+        $('#legalSearch').autocomplete({
+            lookup: legalContent,
+            onSelect: function(suggestion) {
+                $('#legalResult').empty().html('<h4 id="modalTitle">' + suggestion.value + '</h4><p id="modalBody">' + suggestion.data.modalBody + '<br><br><span id="modalID">' + suggestion.data.ID + '</span></p>');
             }
         });
     }

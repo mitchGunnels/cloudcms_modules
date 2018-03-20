2f2d51ce60dbe9499e1ba265db7ef9a989d33102
Mitchell Gunnels <mg717k@att.com>
2018-03-20 17:01:47 -0400

    test

--- preview-url/script/preview.js
+++ preview-url/script/preview.js
@@ -6,6 +6,7 @@ define(function(require, exports, module) {
         //WHEN "CREATING A DOCUMENT" SELECT THE PROPER NODE BASED ON THE CURRENT FOLDER
         var nodeName = $('ol.breadcrumb.docpath li:last a').text().replace(/\s/g, '').replace(/_/g, ' ');
         $('.list-button-new_document').on('click', function(event) {
+            console.log(event);
             var selectExists = setInterval(function() {
                 if($('.card-content-holder select').length){
                     $('.card-content-holder select option:contains(' + nodeName + ')').attr('selected', 'selected');

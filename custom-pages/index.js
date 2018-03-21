d1c35a7d799cdb3b3d38e5b12eb6f1fecfea6ba1
Mitchell Gunnels <mg717k@att.com>
2018-03-21 16:35:55 -0400

    test

--- custom-pages/index.js
+++ custom-pages/index.js
@@ -4,19 +4,19 @@ define(function(require) {
     require("./gadgets/sample-products-list/sample-products-list.js");
 
     // dashlet: "sample-random-product-dashlet"
-    require("./gadgets/sample-random-product-dashlet/sample-random-product-dashlet.js");
+    //require("./gadgets/sample-random-product-dashlet/sample-random-product-dashlet.js");
 
     // action: "create-sample-space"
-    require("./actions/sample/create-sample-space.js");
+    //require("./actions/sample/create-sample-space.js");
 
     // sample form fields
-    require("./fields/sample-city-picker.js");
-    require("./fields/sample-content-picker.js");
+    //require("./fields/sample-city-picker.js");
+    //require("./fields/sample-content-picker.js");
 
     // sample override to document-properties
-    require("./gadgets/sample-product-document-properties/sample-product-document-properties.js");
+    //require("./gadgets/sample-product-document-properties/sample-product-document-properties.js");
 
     // global CSS overrides
-    require("css!./styles/sample.css");
+    //require("css!./styles/sample.css");
 
 });

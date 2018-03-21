2f1b79854f04b5b6ac674ab52aa510774126a00e
Mitchell Gunnels <mg717k@att.com>
2018-03-21 15:32:40 -0400

    test

--- custom-pages/index.js
+++ custom-pages/index.js
@@ -2,8 +2,5 @@ define(function(require) {
 
     // page: "my-blogs-list"
     require("./gadgets/my-blogs-list/my-blogs-list.js");
-
-    // global CSS overrides
-    require("css!./styles/my-blogs-list.css");
-
+    
 });

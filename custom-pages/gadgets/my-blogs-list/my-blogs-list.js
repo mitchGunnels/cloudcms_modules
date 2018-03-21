4e6a1f83e7c784be3884bbd5439db7b584721cb0
Mitchell Gunnels <mg717k@att.com>
2018-03-21 15:42:24 -0400

    test

--- custom-pages/gadgets/my-blogs-list/my-blogs-list.js
+++ custom-pages/gadgets/my-blogs-list/my-blogs-list.js
@@ -35,33 +35,33 @@ define(function(require, exports, module) {
             var branch = this.observable("branch").get();
 
             // call into base method and then set up the model
-            this.base(el, model, function() {
+            // this.base(el, model, function() {
 
-                // query for blog post instances
-                branch.queryNodes({ "_type": "mmcx:blogpost", "blogState": {"$in": ["In Progress", "New"]} }, {"limit": 50}).then(function() {
+            //     // query for blog post instances
+            //     branch.queryNodes({ "_type": "mmcx:blogpost", "blogState": {"$in": ["In Progress", "New"]} }, {"limit": 50}).then(function() {
 
-                    // store blog post node on the model (as a list) and then fire callback
-                    model.nodes = this.asArray();
-                    model.harry = "hello";
+            //         // store blog post node on the model (as a list) and then fire callback
+            //         model.nodes = this.asArray();
+            //         model.harry = "hello";
 
-                    // add "imageUrl" attribute
-                    // add "browseUrl" attribute
-                    for (var i = 0; i < model.nodes.length; i++)
-                    {
-                        var node = model.nodes[i];
+            //         // add "imageUrl" attribute
+            //         // add "browseUrl" attribute
+            //         for (var i = 0; i < model.nodes.length; i++)
+            //         {
+            //             var node = model.nodes[i];
 
-                        node.imageUrl256 = "/preview/repository/" + node.getRepositoryId() + "/branch/" + node.getBranchId() + "/node/" + node.getId() + "/default?size=256&name=preview256&force=true";
-                        node.imageUrl128 = "/preview/repository/" + node.getRepositoryId() + "/branch/" + node.getBranchId() + "/node/" + node.getId() + "/default?size=128&name=preview128&force=true";
-                        node.browseUrl = "/#/projects/" + project._doc + "/documents/" + node._doc + "/properties/";
+            //             node.imageUrl256 = "/preview/repository/" + node.getRepositoryId() + "/branch/" + node.getBranchId() + "/node/" + node.getId() + "/default?size=256&name=preview256&force=true";
+            //             node.imageUrl128 = "/preview/repository/" + node.getRepositoryId() + "/branch/" + node.getBranchId() + "/node/" + node.getId() + "/default?size=128&name=preview128&force=true";
+            //             node.browseUrl = "/#/projects/" + project._doc + "/documents/" + node._doc + "/properties/";
 
-                        console.log(node.author.title);
-                        console.log(node.headline);
-                        console.log(node.blogState);
-                    }
+            //             console.log(node.author.title);
+            //             console.log(node.headline);
+            //             console.log(node.blogState);
+            //         }
 
-                    callback();
-                });
-            });
+            //         callback();
+            //     });
+            // });
         },
 
         /**
@@ -90,24 +90,24 @@ define(function(require, exports, module) {
          */
         afterSwap: function(el, model, originalContext, callback)
         {
-            this.base(el, model, originalContext, function() {
+            // this.base(el, model, originalContext, function() {
 
-                // find all .media-popups and attach to a lightbox
-                $(el).find(".media-popup").click(function(e) {
+            //     // find all .media-popups and attach to a lightbox
+            //     $(el).find(".media-popup").click(function(e) {
 
-                    e.preventDefault();
+            //         e.preventDefault();
 
-                    var nodeIndex = $(this).attr("data-media-index");
-                    var node = model.nodes[nodeIndex];
+            //         var nodeIndex = $(this).attr("data-media-index");
+            //         var node = model.nodes[nodeIndex];
 
-                    UI.showPopupModal({
-                        "title": "Viewing: " + node.title,
-                        "body": "<div style='text-align:center'><img src='" + node._doc + "'></div>"
-                    });
-                });
+            //         UI.showPopupModal({
+            //             "title": "Viewing: " + node.title,
+            //             "body": "<div style='text-align:center'><img src='" + node._doc + "'></div>"
+            //         });
+            //     });
 
-                callback();
-            });
+            //     callback();
+            // });
         }
 
     }));

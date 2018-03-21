9742d834474ef0cf2728ef82c2977a52db834a93
Mitchell Gunnels <mg717k@att.com>
2018-03-20 21:58:35 -0400

    s

--- ckeditor-mods/script/ckeditor-mods.js
+++ ckeditor-mods/script/ckeditor-mods.js
@@ -8,10 +8,10 @@ define(function(require, exports, module) {
     uri = uri.substring(0, uri.lastIndexOf('/'));
 
     require('https://cache.cricketwireless.com/ckeditor-plugins/jquery.autocomplete.min.js');
-    var FleschKincaid = require('https://cache.cricketwireless.com/ckeditor-plugins/flesch-kincaid.js'); 
+    require('https://cache.cricketwireless.com/ckeditor-plugins/flesch-kincaid.js'); 
 
     var str = "The quick brown fox jumped over the lazy dogs";
-    console.log( FleschKincaid); // 3.653333333333336
+    console.log(textStatistics); // 3.653333333333336
 
     CKEDITOR.config.skin = 'moono-lisa';
     CKEDITOR.config.customConfig = '';

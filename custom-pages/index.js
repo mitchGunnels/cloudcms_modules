e838842ad8ef414d79f53910937ceea8b7d3270f
Mitchell Gunnels <mg717k@att.com>
2018-03-21 16:57:26 -0400

    test

--- custom-pages/index.js
+++ custom-pages/index.js
@@ -1,21 +1,4 @@
 define(function(require) {
-
     // page: "sample-products-list"
     require("./gadgets/sample-products-list/sample-products-list.js");
-
-    // dashlet: "sample-random-product-dashlet"
-    //require("./gadgets/sample-random-product-dashlet/sample-random-product-dashlet.js");
-
-    // action: "create-sample-space"
-    //require("./actions/sample/create-sample-space.js");
-
-    // sample form fields
-    //require("./fields/sample-city-picker.js");
-    //require("./fields/sample-content-picker.js");
-
-    // sample override to document-properties
-    //require("./gadgets/sample-product-document-properties/sample-product-document-properties.js");
-
-    // global CSS overrides
-    //require("css!./styles/sample.css");
 });

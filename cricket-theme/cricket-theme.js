61efa1c992fb83857daa2b72b8a206747d128ab4
Mitchell Gunnels <mg717k@att.com>
2018-03-25 21:20:48 -0400

    theme

--- cricket-theme/cricket-theme.js
+++ cricket-theme/cricket-theme.js
@@ -1,8 +1,6 @@
 define(function(require, exports, module) {
     var UI = require("ui");
-
     require("css!bootstrap/../../css/bootstrap.css");
     require("css!app/../../main.css");
-
     require("css!./cricket-theme.css");
 });

145894c89bd1e7414b3c6e378eaa4ecee2f17d44
nc223x <nc223x@att.com>
2019-07-11 13:51:34 -0400

    fix an ooops

--- page/index.js
+++ page/index.js
@@ -1,5 +1,5 @@
 define(function(require, exports, module) {
 	//var moduleId = module.uri.match(/^.+(_modules[^\/]+)\/.*/)[1];
 	//console.log('module', module);
-    require("./script/preview.js");
+    require("./script/page.js");
 });

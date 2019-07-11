0eddd6aea95b374e188742dfe2d2190b2f8a4e25
nc223x <nc223x@att.com>
2019-07-11 13:47:37 -0400

    add new module, verify running via console.error

---
+++ page/script/page.js
@@ -0,0 +1,10 @@
+define(function(require, exports, module) {
+  //detect if current page is page edit properties (extend to handle descendents)
+  //if not, do nothing and exit
+  
+  console.error("PAGE  BOZ")
+  //inject button next to URL field
+  //Query for other pages (or other types defined to check for) with URL set to the current value
+  //If any are found, indicate invalid
+  //Provide links to other pages?
+});
---
+++ page/script/preview.js
@@ -0,0 +1,109 @@
+define(function(require, exports, module) {
+    var $ = require("jquery");
+    $(document).on('cloudcms-ready', function(event) {
+        //THIS IS TO CHANGE THE DEFAULT CLICK TO TAKE USERS TO THE PROPERTIES PAGE. QUICKER EDITING
+        var propLink = setInterval(function() {
+            if ($('.documents-list table td h2 a').length > 0 || $('.content-instances table td h2 a').length > 0) {
+                $('.list-row-info.title a').each(function(index, el) {
+                    var url = $(this).attr('href');
+                    var slug = url.substr(url.lastIndexOf('/') + 1);
+                    var self = this;
+                    if (slug != 'browse' && slug != 'properties') {
+                        $(self).attr('href', url + '/properties');
+                    }
+                });
+                //console.log('attempt');
+                clearInterval(propLink);
+            }
+        }, 50);
+
+        //Adding just in case the interval doesn't clear. 
+        setTimeout(function() { clearInterval(propLink); }, 5000);
+
+        //HANDLE THE COLOR AT THE TOP OF THE PAGE TO REMIND WHICH BRANCH
+        var workspacePickerVal = $('select.workspace-picker option:selected').text();
+        if (workspacePickerVal.includes("Master")) {
+            $('div[region="workspace-bar"]').css('borderTopColor', '#a94442');
+        } else {
+            $('div[region="workspace-bar"]').css('borderTopColor', '#60a630');
+        }
+        $('select.workspace-picker').on('change', function() {
+            var workspacePickerVal = $('select.workspace-picker option:selected').text();
+            if (workspacePickerVal.includes("Master")) {
+                $('div[region="workspace-bar"]').css('borderTopColor', '#a94442');
+            } else {
+                $('div[region="workspace-bar"]').css('borderTopColor', '#60a630');
+            }
+        });
+
+        //------------------------------------------------------------------
+
+        //WHEN "CREATING A DOCUMENT" SELECT THE PROPER NODE BASED ON THE CURRENT FOLDER
+        // $(document).on('click', '.list-button-new_document', function(event) {
+        //     var nodeName = $('ol.breadcrumb.docpath li:last a').text().replace(/\s/g, '').replace(/_/g, ' ');
+        //     var selectExists = setInterval(function() {
+        //         if ($('.card-content-holder select').length) {
+        //             setTimeout(function(){$('.card-content-holder select option:contains(' + nodeName + ')').attr('selected', 'selected');},10)
+        //             clearInterval(selectExists);
+        //         }
+        //     }, 50);
+        // });
+
+        //Create custom config for authors
+        var uiConfig = $('button[data-header-item-key=ui-config-dropdown]').text().replace(/\s/g, '');
+        if (uiConfig == 'Author') {
+            //HIDE FROM VIEW
+            $('.list-button-view_details, .list-button-new_text_document').parent().hide();
+            $('.list-button-view_rules, .dropdown-menu li.divider').hide();
+            //CHANGE NAME
+
+            //CUSTOM ACTIONS
+        }
+
+        //---------------------------------------------------------------------------------------------------------------
+        //IF DELAY IS NEEDED:
+        setTimeout(function() {
+            
+            //Check workspace-picker to determine the appropriate env for creating the preview link.
+            var workspacePickerVal = $('select.workspace-picker option:selected').text();
+            var domain;
+            //Decide on the correct environment
+            if (workspacePickerVal.includes("Master")) {
+                //console.log("prod");
+                domain = "https://www.cricketwireless.com";
+            } else if (workspacePickerVal.includes("SIT1")) {
+                //console.log("SIT1");
+                domain = "https://wwwsit1.cricketwireless.com";
+            } else if (workspacePickerVal.includes("SIT2")) {
+                //console.log("SIT2");
+                domain = "https://wwwsit2.cricketwireless.com";
+            } else if (workspacePickerVal.includes("SIT3")) {
+                //console.log("SIT3");
+                domain = "https://wwwsit3.cricketwireless.com";
+            } else if (workspacePickerVal.includes("SIT6")) {
+                //console.log("SIT6");
+                domain = "https://wwwsit6.cricketwireless.com";
+            } else if (workspacePickerVal.includes("SIT8")) {
+                //console.log("SIT8");
+                domain = "https://wwwsit8.cricketwireless.com";
+            } else if (workspacePickerVal.includes("SIT9")) {
+                //console.log("SIT9");
+                domain = "https://wwwsit9.cricketwireless.com";
+            } else {
+                //console.log("Nothing found");
+            }
+            var endPoint = $('div[name=previewURL]').text();
+            var inputEndPoint = $('input[name=previewURL]').val();
+            $('.previewButton').remove();
+            if (endPoint != undefined && endPoint.length > 0) {
+                $('div[name=previewURL]').append(' <span class="previewButton">- <a href="' + domain + endPoint + '" target="_blank">Preview Content</a></span>');
+                $('.content-holder div.row:first div.col-md-4').prepend('<div class="pull-right previewButton"><a href="' + domain + endPoint + '" class="btn btn-success" target="_blank"><span class="fa fa-eye" aria-hidden="true"></span> Preview Content</a></div>');
+            } else if (inputEndPoint != undefined && inputEndPoint.length > 0) {
+                $('div[name=previewURL]').append(' <span class="previewButton">- <a href="' + domain + inputEndPoint + '" target="_blank">Preview Content</a></span>');
+                $('.content-holder div.row:first div.col-md-4').prepend('<div class="pull-right previewButton"><a href="' + domain + inputEndPoint + '" class="btn btn-success" target="_blank"><span class="fa fa-eye" aria-hidden="true"></span> Preview Content</a></div>');
+            }
+
+        }, 2000);
+
+    });
+});

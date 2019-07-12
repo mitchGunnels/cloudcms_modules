ef528c219799c17e7642c760be65485a10ab2155
nc223x <nc223x@att.com>
2019-07-12 16:22:05 -0400

    add query, trigger on cloudcms-ready event, and keyup/paste in url text field. Disable create/save buttons in Create wizard/Edit Properties pages

--- page/script/page.js
+++ page/script/page.js
@@ -1,10 +1,76 @@
 define(function(require, exports, module) {
-  //detect if current page is page edit properties (extend to handle descendents)
-  //if not, do nothing and exit
+  var $ = require("jquery");
+  var saveButtonSelector = ".buttonbar .btn.save, .btn-toolbar .btn.save, .btn.btn-success.wizard-next"
+  var urlFieldSelector = ".alpaca-field [data-alpaca-field-name=urlList_0_url]"
+  var urlTextSelector = ".alpaca-field [name=urlList_0_url]"
+  var createContentButtonSelector = ".list-button-create-content"
+  var branch = Ratchet.observable('branch').get()
+  var chain = Chain(branch).trap(genericErrorLoggerHalter)
+  var latestPagesRequestTime
+  var typing = false
+  var docId = null
+
+  function setInvalidUrl() {
+    $(saveButtonSelector).attr("disabled", "disabled")
+  }
   
-  console.error("PAGE  BOZ")
-  //inject button next to URL field
-  //Query for other pages (or other types defined to check for) with URL set to the current value
-  //If any are found, indicate invalid
-  //Provide links to other pages?
+  function setValidUrl() {
+    $(saveButtonSelector).removeAttr("disabled")
+  }
+
+  function genericErrorLoggerHalter(err) {
+    console.error(err);
+    return false;
+  }
+
+  function queryForPages(url) {
+    return chain.queryNodes({
+      _type: {
+        $regex: "cricket:page(-.*)?"
+      },
+      _doc: {
+        $ne: docId
+       
+      },
+      urlList: {
+        $elemMatch: {
+          url: url
+        }
+      },
+      active: 'y'
+    })
+  }
+
+  function findIdenticalUrlPages() {
+    latestPagesRequestTime = new Date().getTime()
+    var pagesRequestTime = new Date().getTime()
+    var url = $(urlTextSelector).val()
+
+    queryForPages(url).then(function () {
+      //ensure only final check results in DOM update
+      if (pagesRequestTime === latestPagesRequestTime) {
+        var identicalUrlPages = this.asArray()
+        var pageCount = identicalUrlPages.length
+        if (pageCount) {
+          setInvalidUrl()
+        } else {
+          setValidUrl()
+        }
+      }
+    })
+  }
+
+  $(document).on('cloudcms-ready', function(event) {
+    $(urlTextSelector).off()
+    //detect if current page is edit properties
+    var pagePattern = /^.*\/documents\/(\w+)\/properties$/
+    var isPage = pagePattern.test(location.href)
+    if (isPage) {
+      docId = location.href.replace(pagePattern, '$1')
+      setInvalidUrl()
+      findIdenticalUrlPages()
+    }
+
+    $(document).on('keyup paste', urlTextSelector, findIdenticalUrlPages)
+  })
 });

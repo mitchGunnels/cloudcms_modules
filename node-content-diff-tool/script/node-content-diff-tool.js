70e204bf8591d7ff751173b4747fec67208279f5
Gray, Benjamin (bg7757) <bg7757@att.com>
2019-08-06 12:47:05 -0400

    adds stylesheet

--- node-content-diff-tool/script/node-content-diff-tool.js
+++ node-content-diff-tool/script/node-content-diff-tool.js
@@ -1,6 +1,14 @@
-define(function(require, exports, module) {
+define(["xstyle!./style.css"], function(require, exports, module) {
     var $ = require("jquery");
     $(document).on('cloudcms-ready', function(event) {
+        // append new option to select dropdown
 
+        // when click on new option fetch the old node, store in variable
+
+        // then fetch the new node, store in variable
+
+        // use difftool to compare the two variables
+
+        // open new tab with the contents of the two docs
     });
 });

ead88baa34321dd72d8b930684a88bd9d3aff966
Gray, Benjamin (bg7757) <bg7757@att.com>
2019-08-06 12:09:24 -0400

    removes scripts dir and removes console.log

--- node-content-diff-tool/script/node-content-diff-tool.js
+++ node-content-diff-tool/script/node-content-diff-tool.js
@@ -1,6 +1,6 @@
 define(function(require, exports, module) {
     var $ = require("jquery");
     $(document).on('cloudcms-ready', function(event) {
-        console.log('You are here');
+
     });
 });
--- node-content-diff-tool/scripts/node-content-diff-tool.js
+++
@@ -1,6 +0,0 @@
-define(function(require, exports, module) {
-    var $ = require("jquery");
-    $(document).on('cloudcms-ready', function(event) {
-
-    });
-});

ae53bb43b06920bf4d6384ad57599f30add50a17
Tenorio Arzola, Juan (jt044c) <jt044c@us.att.com>
2019-08-07 14:12:25 -0400

    Adding logging to see where we get.
    Signed-off-by: Tenorio Arzola, Juan (jt044c) <jt044c@us.att.com>

--- custom-pages/gadgets/admin-tools/admin-tools.js
+++ custom-pages/gadgets/admin-tools/admin-tools.js
@@ -1,4 +1,4 @@
-define(function(require, exports, module) {
+define(function (require, exports, module) {
     
     require('css!./styles/admin-tools.css');
     const html = require('text!./templates/admin-tools.html');
@@ -8,16 +8,17 @@ define(function(require, exports, module) {
     const UI = require('ui');
     
     return UI.registerGadget('admin-tools', Empty.extend({
-
+    
         TEMPLATE: html,
-
+    
         /**
          * Binds this gadget to the /admin-tools route
          */
-        setup: function() {
+        setup: function () {
+            console.log('setup()');
             this.get('/projects/{projectId}/admin-tools', this.index);
         },
-
+    
         /**
          * Puts constiables into the model for rendering within our template.
          * Once we've finished setting up the model, we must fire callback().
@@ -26,28 +27,28 @@ define(function(require, exports, module) {
          * @param model
          * @param callback
          */
-        prepareModel: function(el, model, callback) {
-
+        prepareModel: function (el, model, callback) {
+            
             // get the current project
             const project = this.observable('project').get();
             const branch = this.observable('branch').get();
             const repository = branch.getRepository();
-
+        
             console.log(Gitana.MergeConflict(repository));
-
+        
             console.log('project', project);
             console.log('branch ', branch);
             console.log('repository ', repository);
             console.log('model', model);
-
+        
             // call into base method and then set up the model
-            this.base(el, model, function() {
-
+            this.base(el, model, function () {
+                console.log('prepareModel()');
                 callback();
-
+            
             });
         },
-
+    
         /**
          * This method gets called before the rendered DOM element is injected into the page.
          *
@@ -55,15 +56,14 @@ define(function(require, exports, module) {
          * @param model the model used to render the template
          * @param callback
          */
-        /*
-        beforeSwap: function(el, model, callback)
-        {
-            this.base(el, model, function() {
+        beforeSwap: function (el, model, callback) {
+            this.base(el, model, function () {
+                console.log('beforeSwap()');
                 callback();
             });
         },
-        */
-
+    
+    
         /**
          * This method gets called after the rendered DOM element has been injected into the page.
          *
@@ -72,13 +72,13 @@ define(function(require, exports, module) {
          * @param originalContext the dispatch context used to inject
          * @param callback
          */
-        afterSwap: function(el, model, originalContext, callback) {
-            this.base(el, model, originalContext, function() {
-    
+        afterSwap: function (el, model, originalContext, callback) {
+            this.base(el, model, originalContext, function () {
+                console.log('afterSwap()');
                 callback();
             });
         }
-
+    
     }));
-
+    
 });

27532b560975b5913bcaad2706793e7dc7d0d051
Tenorio Arzola, Juan (jt044c) <jt044c@us.att.com>
2019-08-07 13:59:34 -0400

    Makes sure to have an index template and css file as well
    Signed-off-by: Tenorio Arzola, Juan (jt044c) <jt044c@us.att.com>

--- custom-pages/gadgets/admin-tools/admin-tools.js
+++ custom-pages/gadgets/admin-tools/admin-tools.js
@@ -40,62 +40,11 @@ define(function(require, exports, module) {
             console.log('repository ', repository);
             console.log('model', model);
 
-            repository.queryChangesets({
-                "branch": branch._doc
-            }).each(function() {
-                console.log("Found changeset: " + this.getId());
-    
-                const changesetId = this.getId();
-
-                repository.listChangesetChildren(changesetId).each(function() {
-                    console.log("The changeset: " + changesetId + " has child: " + this.getId());
-                });
-
-                // branch.readNode("8bc807d9b2fcf6621df3").then(function() {
-                //     console.log("Found node: " + this.get("title"));
-                // });
-
-
-            });
-
-
             // call into base method and then set up the model
             this.base(el, model, function() {
 
-                //model.project = project._doc;
-                //model.branch = branch._doc;
-
-
-                //console.log('repo', this.observable("repository")._doc);
-                //console.log('repo', this.observable("repository").id);
-
-                // assume we have a repository
-                //const repository = "fe6166ac8042240542f8";
-                // here is our branch id
-                //const branchId = branch._doc;
-
-
                 callback();
 
-                // query for catalog:product instances
-                // branch.queryNodes({ "_type": "catalog:product" }).then(function() {
-
-                //     // store "products" on the model (as a list) and then fire callback
-                //     model.products = this.asArray();
-
-                //     // add "imageUrl" attribute to each product
-                //     // add "browseUrl" attribute to each product
-                //     for (const i = 0; i < model.products.length; i++)
-                //     {
-                //         const product = model.products[i];
-
-                //         product.imageUrl256 = "/preview/repository/" + product.getRepositoryId() + "/branch/" + product.getBranchId() + "/node/" + product.getId() + "/default?size=256&name=preview256&force=true";
-                //         product.imageUrl128 = "/preview/repository/" + product.getRepositoryId() + "/branch/" + product.getBranchId() + "/node/" + product.getId() + "/default?size=128&name=preview128&force=true";
-                //         product.browseUrl = "/#/projects/" + project._doc + "/documents/" + product._doc;
-                //     }
-
-                //     callback();
-                // });
             });
         },
 
@@ -125,21 +74,7 @@ define(function(require, exports, module) {
          */
         afterSwap: function(el, model, originalContext, callback) {
             this.base(el, model, originalContext, function() {
-
-                // find all .media-popups and attach to a lightbox
-                // $(el).find(".media-popup").click(function(e) {
-
-                //     e.preventDefault();
     
-                //     const productIndex = $(this).attr("data-media-index");
-                //     const product = model.products[productIndex];
-
-                //     UI.showPopupModal({
-                //         "title": "Viewing: " + product.title,
-                //         "body": "<div style='text-align:center'><img src='" + product.imageUrl256 + "'></div>"
-                //     });
-                // });
-
                 callback();
             });
         }

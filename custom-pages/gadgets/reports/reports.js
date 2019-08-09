86a5ebd72494ee5d58e373263857eb73d29ab759
nc223x <nc223x@att.com>
2019-08-09 18:42:56 -0400

    hide other buttons and correct csv generation for page report button

--- custom-pages/gadgets/reports/reports.js
+++ custom-pages/gadgets/reports/reports.js
@@ -6,38 +6,109 @@ define(function (require, exports, module) {
     const Empty = require('ratchet/dynamic/empty');
     
     const UI = require('ui');
+    const $ = require('jquery')
+
+    const [PAGE, PRODUCT, SKU, PRICESHEET] = [
+        'page',
+        'product',
+        'sku',
+        'price-sheet'
+    ]
+    const ReportBtnClasses = [PAGE, PRODUCT, SKU, PRICESHEET]
+
+    const QueryTypes = {
+        PAGE: {
+            "$regex": "cricket:page(-.+)?"
+        },
+        PRODUCT: "cricket:product"
+    }
     
     let branch;
+    let platform;
 
-    function buildQuery(report) {
-        let query = {}
-        switch (report) {
-            case 'page':
-                query._type = {
-                    "$regex": "cricket:page(-.+)?"
-                }
+    function buildQuery(reportType) {
+        let query = { }
+        switch (reportType) {
+            case PAGE:
+                query._type = QueryTypes.PAGE,
                 query._fields = {
                     title: 1,
                     _type: 1,
-                    url: 1,
+                    "urlList.0.url": 1,
                     active: 1,
-                    _system: {
-                        created_on: 1,
-                        created_by: 1,
-                        edited_on: 1,
-                        edited_by: 1,
-                    }
+                    "_system.created_on": 1,
+                    "_system.created_by": 1,
+                    "_system.edited_on": 1,
+                    "_system.edited_by": 1
                 }
                 break
-            case 'product':
-                query._type = "cricket:product"
+            case PRODUCT:
+                query._type = QueryTypes.PRODUCT
                 break
             
             default:
                 console.error('Invalid report type provided')
         }
+
+        if (query._type) {
+            return query
+        } else {
+            return false
+        }
     }
 
+    function genericErrorLoggerHalter(err) {
+        console.error(err)
+        return false
+    }
+
+    function exportReport(reportType) {
+        let query = buildQuery(reportType)
+        if (query) {
+            let nodes
+            let exportId
+
+            Chain(branch).trap(genericErrorLoggerHalter).queryNodes(query, {limit: -1}).then(function() {
+                nodes = this
+                this.subchain(platform).trap(genericErrorLoggerHalter).runExport(nodes, {
+                    package: "CSV",
+                    includeMetadata: true,
+                    fields: [                        
+                        "title",
+                        "_type",
+                        "urlList[0]/url",
+                        "active",
+                        "_system/created_on/timestamp",
+                        "_system/created_by",
+                        "_system/edited_on/timestamp",
+                        "_system/edited_by"
+                    ]
+                }, function (_exportId, _status) {
+                    window.location.href = "/proxy/ref/exports/" + _exportId + "/download?a=true&download=" + reportType + ".csv"
+                })
+
+            })
+        }
+    }
+
+    function handleReportButtonClick() {
+        if(branch) {
+            let btn = $(this)
+            let report
+            ReportBtnClasses.forEach(function (reportClass) {
+                if (btn.hasClass(reportClass)) {
+                    report = reportClass
+                }
+            })
+            if (report) {
+                exportReport(report)
+            } else {
+                console.error('Invalid report type provided')
+            }
+        }
+    }
+
+    $(document).on('click', '.reports-btn-group .btn', handleReportButtonClick)
     
     return UI.registerGadget('reports', Empty.extend({
         
@@ -63,6 +134,7 @@ define(function (require, exports, module) {
             
             // get the current project
             branch = this.observable('branch').get();
+            platform = branch.getRepository().getPlatform()
             
             // call into base method and then set up the model
             this.base(el, model, function () {
@@ -97,22 +169,13 @@ define(function (require, exports, module) {
          */
         afterSwap: function (el, model, originalContext, callback) {
             this.base(el, model, originalContext, function () {
-                console.log('afterSwap()');
                 
-                // eslint-disable-next-line no-undef
-                $(el).find('.btn.btn-primary').click(function (e) {
-                    
-                    e.preventDefault();
-                    
-                    UI.showPopupModal({
-                        'title': 'Running report for: ' + branch.getTitle().toUpperCase(),
-                        'body': '<div style="text-align:center">I am fancy</div>'
-                    });
-                    callback();
-                });
             });
         }
         
     }));
     
 });
+
+
+
